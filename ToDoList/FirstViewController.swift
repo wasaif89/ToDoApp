285af6126d1b2cc31dc974439ea800c5055864c1
wasaif89 <ahmedmansour3@hotmail.com>
2021-11-02 16:59:07 +0300

    update edite name (save) to (update)

--- ToDoList/FirstViewController.swift
+++ ToDoList/FirstViewController.swift
@@ -24,13 +24,7 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
         return Cell
     }
     override func viewWillAppear(_ animated: Bool) {
-//        let itemObject = UserDefaults.standard.object(forKey: "list-items")
-//        print(itemObject)
-//        
-//        if let item = itemObject as? [TaskItem] { //st
-//            list.items = item
-//        }
-//        
+       
         table.reloadData()
     }
     @IBAction func deleteAll(_ sender: Any) {
@@ -42,8 +36,7 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//tableview
-        // Do any additional setup after loading the view.
+
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
@@ -55,36 +48,24 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
         //UserDefaults.standard.set(list.items, forKey: "list-items")
     }
     
-    
-   // func moveItem(from: IndexSet, to: Int){
-   //     items.move.(fromOffsets: from, toOffset: to)
-   //}
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("User selected: \(indexPath.row)")
         userSelectedCell = indexPath.row
         performSegue(withIdentifier: "editSegue", sender: self)
+        
     }
    
 
-   // func edit(changes: , indexPath.row: Int){
-   //     items[indexPath.row] = changes
-   //     table.reloadData()
-  // }
-   
-    
-    // func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-       // if editingStyle == UITableViewCell.EditingStyle.update {
-          //  items.[indexPath.row]
-   // }
+  
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (segue.identifier == "addSegue") {
             let secVC = segue.destination as! SecondViewController
             secVC.list = list
             
         } else if(segue.identifier == "editSegue") {
-            let thirdVC2 = segue.destination as! ThirdViewController
-            thirdVC2.list = list
-            thirdVC2.selectedIndex = userSelectedCell
+            let thirdVC = segue.destination as! ThirdViewController
+            thirdVC.list = list
+            thirdVC.selectedIndex = userSelectedCell
 }
     }
 
--- ToDoList/SecondViewController.swift
+++ ToDoList/SecondViewController.swift
@@ -16,28 +16,16 @@ class SecondViewController: UIViewController {
     @IBOutlet weak var noteTextField: UITextField!
     @IBAction func add(_ sender: Any) {
         
-//        let itemObject = UserDefaults.standard.object(forKey: "list-items")
-        //st
-//        if var item = itemObject as? TaskItem {
+
             let item = TaskItem(title: itemTextField.text!, note: noteTextField.text!)
             list.items.append(item)
-//        }else {
-//            lists.items = [itemObject]
-//        }
+
         
     //  UserDefaults.standard.set(list.items, forKey: "list-items")
         navigationController?.popViewController(animated: true)
         
         
-        
-//        if let item = itemObject as? [TaskItem] {
-//            lists.items = item
-//            lists.items.append(noteTextField.text!)
-//        }else {
-//            lists.items = [noteTextField.text!]
-//        }
-//        UserDefaults.standard.set(lists.items, forKey: "list-items")
-//       navigationController?.popViewController(animated: true)
+ 
         
     }
     
